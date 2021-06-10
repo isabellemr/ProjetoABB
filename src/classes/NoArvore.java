65acd46a8e7df13efa35e351cb73aaf0fbbae681
isabelle_ramalho <isabelle-ramalho@outlook.com>
2021-06-09 21:55:03 -0300

    Adição das classes: Principal e NoArvore

--- src/classes/NoArvore.java
+++ src/classes/NoArvore.java
@@ -1,4 +1,91 @@
 package classes;
 
 public class NoArvore {
+    int			valor;
+    NoArvore	direita;
+    NoArvore	esquerda;
+
+    public NoArvore(){
+        this.valor = 0;
+        this.direita = null;
+        this.esquerda = null;
+    }
+    public NoArvore(int novovalor){
+        this.valor = novovalor;
+        this.direita = null;
+        this.esquerda = null;
+    }
+
+    public NoArvore busca(NoArvore no, int valorprocurado) {
+        if(no == null)
+            return null;
+
+        if(no.valor > valorprocurado)
+            return busca(no.esquerda, valorprocurado);
+        else if(no.valor < valorprocurado)
+            return busca(no.direita, valorprocurado);
+        else
+            return no;
+    }
+
+    public NoArvore insere(NoArvore no, int novovalor) {
+        if(no == null) {
+            no = new NoArvore();
+            no.valor = novovalor;
+            no.esquerda = no.direita = null;
+        }
+        else if(novovalor < no.valor)
+            no.esquerda = insere(no.esquerda, novovalor);
+        else if(novovalor > no.valor)
+            no.direita = insere(no.direita, novovalor);
+
+        return no;
+    }
+
+    public void imprime(NoArvore raiz) {
+        if (raiz != null) {
+            imprime(raiz.esquerda);
+            System.out.println(raiz.valor);
+            imprime(raiz.direita);
+        }
+    }
+
+    public NoArvore remove(NoArvore raiz, int valoraremover) {
+        // faz a busca pelo valor a ser removido
+        if (raiz == null)
+            return null;
+        else if (raiz.valor > valoraremover)
+            raiz.esquerda = remove(raiz.esquerda, valoraremover);
+        else if (raiz.valor < valoraremover)
+            raiz.direita = remove(raiz.direita, valoraremover);
+        else {	// passar por aqui significa que achou o n� com o
+            // valora remover procurado e agora vai remov�-lo
+            // segundo as 4 situa��es a seguir:
+            // N�o ter filhos (esquerda e direita == null)
+            if (raiz.esquerda == null && raiz.direita == null) {
+                raiz = null;
+            }
+            // Ter filho apenas � direita (esquerda == null)
+            else if (raiz. esquerda == null) {
+                raiz = raiz.direita;
+            }
+            // Ter filho apenas � esquerda (direita == null)
+            else if (raiz.direita == null) {
+                raiz = raiz.esquerda;
+            }
+            // Ter dois filhos (esquerda e direita != null)
+            else {
+                NoArvore sub_esquerda = raiz.esquerda;
+                // encontrar o n� com maior valor na sub�rvore esquerda
+                while (sub_esquerda.direita != null) {
+                    sub_esquerda = sub_esquerda.direita;
+                }
+                // aqui a sub_esquerda.valor tem o maior valor
+                raiz.valor = sub_esquerda.valor;
+                sub_esquerda.valor = valoraremover;
+                raiz.esquerda = remove(raiz.esquerda, valoraremover);
+            }
+        }
+        return raiz;
+    }
 }
--- src/classes/Principal.java
+++ src/classes/Principal.java
@@ -1,4 +1,63 @@
 package classes;
 
+import classes.NoArvore;
+import java.util.Scanner;
+
 public class Principal {
+
+    public static void main(String[] args) {
+
+        NoArvore abb = new NoArvore();
+
+        Scanner input = new Scanner(System.in);
+        //ATRIBUTOS
+        int menu = 99;
+        int rgm;
+
+        //PROGRAMA
+        while(menu != 0) {
+
+            //MENSAGEM PARA ENTRAR NO MENU PRINCIPAL
+            System.out.println("Digite 1 e aperte enter para inserir um novo aluno. \n" +
+                    "Digite 2 e aperte enter para remover um nó. \n" +
+                    "Digite 3 e aperte enter para pesquisar um aluno. \n" +
+                    "Digite 4 e aperte enter para esvaziar a árvore. \n" +
+                    "Digite 5 e aperte enter para exibir a árvore \n" +
+                    "Digite 0 e aperte enter para finalizar o programa. \n");
+            menu = input.nextInt();
+
+            //MENU PRINCIPAL
+            switch (menu) {
+                //CADASTRO
+                case 1:
+                    System.out.println("Digite o RGM do aluno que deseja cadastrar: ");
+                    rgm = input.nextInt();
+                    abb.insere(abb, rgm);
+                    break;
+
+                case 2:
+                    System.out.println("Digite o RGM que deseja remover: ");
+                    rgm = input.nextInt();
+                    abb.remove(abb, rgm);
+                    break;
+
+                case 3:
+                    System.out.println("Digite o RGM que deseja pesquisar: ");
+                    rgm = input.nextInt();
+                    System.out.println(abb.busca(abb, rgm));
+                    break;
+
+                case 4:
+                    System.out.println("case4");
+                    break;
+
+                case 5:
+                    abb.imprime(abb);
+                    break;
+            }
+
+        }
+        System.out.println("Programa encerrado");
+    }
+
 }
